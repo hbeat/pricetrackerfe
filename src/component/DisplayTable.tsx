7e18c1ff04a1b3f6927842f6d78babd6d6891969
hbeat <49186507+hbeat@users.noreply.github.com>
2023-07-29 23:09:27 +0700

    1 file modified

--- src/component/DisplayTable.tsx
+++ src/component/DisplayTable.tsx
@@ -1,39 +1,55 @@
 import * as React from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 export const DisplayTable = ({ data }) => {
   console.log('data in display table', data);
   const columns: GridColDef[] = [
     // { field: 'id', headerName: 'ID', type: 'number', width: 70 },
-    { field: 'name', headerName: 'Product', type: 'string', width: 70 },
-    { field: 'date', headerName: 'Date', type: 'dateTime', width: 130 },
-    { field: 'source', headerName: 'Source', type: 'string', width: 130 },
+    { field: 'name', headerName: 'Product', type: 'string', width: 100 },
+    {
+      field: 'date',
+      headerName: 'Date',
+      type: 'dateTime',
+      width: 200,
+      valueGetter: (params: GridValueGetterParams) => new Date(params.row.date),
+    },
+    { field: 'source', headerName: 'Source', type: 'string', width: 100 },
     {
       field: 'price',
       headerName: 'Price',
       type: 'number',
-      width: 90,
+      width: 150,
     },
   ];
 
   const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-    data,
+    { id: 1, name: 'ss1', date: '', source: 'lazada', price: 35.0 },
+    { id: 2, name: 'ss1', date: '', source: 'lazada', price: 35.0 },
+    { id: 3, name: 'ss1', date: '', source: 'lazada', price: 35.0 },
+    { id: 4, name: 'ss1', date: '', source: 'lazada', price: 35.0 },
   ];
   console.log(rows, data);
+  const [fetchedData, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://pricetracker-2ed88b8b3e1f.herokuapp.com/v1/product')
+      .then((res) => {
+        console.log('Fetch', res);
+        setData(res.data.data);
+      })
+      .catch((err) => {
+        console.error(err);
+        alert('Can not get data from database');
+      });
+  }, []);
 
   return (
-    <div>
+    <div style={{ width: 'auto' }}>
       Tableeee
       <DataGrid
-        rows={rows}
+        rows={fetchedData}
         columns={columns}
         initialState={{
           pagination: {
@@ -41,8 +57,8 @@ export const DisplayTable = ({ data }) => {
           },
         }}
         pageSizeOptions={[5, 10]}
-        checkboxSelection
       />
+      {/* {JSON.stringify(fetchedData)} */}
     </div>
   );
 };
