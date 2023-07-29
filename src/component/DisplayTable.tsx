e4ce736b6e8b5d45d373bd1c51afda88763aad45
hbeat <49186507+hbeat@users.noreply.github.com>
2023-07-29 22:53:21 +0700

    2 files modified

--- src/component/DisplayTable.tsx
+++ src/component/DisplayTable.tsx
@@ -1,5 +1,48 @@
 import * as React from 'react';
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 export const DisplayTable = ({ data }) => {
   console.log('data in display table', data);
-  return <div>Tableeee</div>;
+  const columns: GridColDef[] = [
+    // { field: 'id', headerName: 'ID', type: 'number', width: 70 },
+    { field: 'name', headerName: 'Product', type: 'string', width: 70 },
+    { field: 'date', headerName: 'Date', type: 'dateTime', width: 130 },
+    { field: 'source', headerName: 'Source', type: 'string', width: 130 },
+    {
+      field: 'price',
+      headerName: 'Price',
+      type: 'number',
+      width: 90,
+    },
+  ];
+
+  const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    data,
+  ];
+  console.log(rows, data);
+
+  return (
+    <div>
+      Tableeee
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+      />
+    </div>
+  );
 };
